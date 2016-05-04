d380e10b573bac396915825d3e8e9c738032e8ff
choco-loo <choco-loo>
2016-05-04 15:57:00 +0100

    Initial commit

---
+++ sflow.php
@@ -0,0 +1,149 @@
+<?php
+
+    class Sflow
+    {
+
+        const INTERVAL = 5;
+
+        private $flows = [];
+        private $counters = [];
+        private $devices = [];
+
+        private $collect = false;
+
+        public function __construct()
+        {
+            $this->options = getopt('', [ 'device:', 'interface:', 'type:' ]);
+
+            if (!isset($this->options['type'])) {
+                printf("Error: --type is required (flow|cntr)\n");
+                exit(1);
+            }
+
+            $this->options['type'] = ucfirst($this->options['type']);
+        }
+
+        private function processCntr($values)
+        {
+            $keys = [ 'device', 'interface', 'type', 'speed', 'direction', 'status',
+                      'in_octets', 'in_ucast_pkts', 'in_mcast_pkts', 'in_bcast_pkts', 'in_discards', 'in_errors', 'in_unknown_proto',
+                      'out_octets', 'out_ucast_pkts', 'out_mcast_pkts', 'out_bcast_pkts', 'out_discards', 'out_errors', 'out_unknown_proto' ];
+
+            $data = array_combine($keys, $values);
+            $data['time'] = time();
+
+            if (!$this->filterDevice($data))
+                return;
+
+            if (!$this->filterInterface($data))
+                return;
+
+            $key = $data['device'].$data['interface'];
+
+            if (!isset($this->counters[$key])) {
+                $this->counters[$key] = $data;
+                return;
+            }
+
+            $interval = $data['time'] - $this->counters[$key]['time'];
+
+            $bps_in = ( $data['in_octets'] - $this->counters[$key]['in_octets'] ) / $interval * 8;
+            $mbps_in = $bps_in / pow(1000, 2);
+            $pps_in = ( $data['in_ucast_pkts'] - $this->counters[$key]['in_ucast_pkts'] + $data['in_mcast_pkts'] - $this->counters[$key]['in_mcast_pkts'] + $data['in_bcast_pkts'] - $this->counters[$key]['in_bcast_pkts']) / 1000 / $interval ;
+
+            $bps_out = ( $data['out_octets'] - $this->counters[$key]['out_octets'] ) / $interval * 8;
+            $mbps_out = $bps_out / pow(1000, 2);
+            $pps_out = ( $data['out_ucast_pkts'] - $this->counters[$key]['out_ucast_pkts'] + $data['out_mcast_pkts'] - $this->counters[$key]['out_mcast_pkts'] + $data['out_bcast_pkts'] - $this->counters[$key]['out_bcast_pkts']) / 1000 / $interval;
+
+            printf("%-12s %-15s %6.2f Mb/s %5d kpps\t%6.2f Mb/s %5d kpps\n",
+                $data['device'],
+                $data['interface'],
+                $mbps_in,
+                $pps_in,
+                $mbps_out,
+                $pps_out);
+
+            $this->counters[$key] = $data;
+            return;
+        }
+
+        private function processFlow($values)
+        {
+            return;
+            $keys = [ 'device', 'input_port', 'output_port', 'src_mac', 'dst_mac', 'ethernet_type', 'in_vlan', 'out_vlan', 'src_ip', 'dst_ip', 'ip_protocol', 'ip_tos', 'ip_ttl', 'src_port_or_icmp_type', 'dst_port_or_icmp_code', 'tcp_flags', 'packet_size', 'ip_size', 'sampling_rate' ];
+
+            $data = array_combine($keys, $values);
+
+            if (!isset($this->flows[$data['device']]))
+                $this->flows[$data['device']] = [ 'bits' => [] ];
+
+            if (!$this->filterDevice($data) || !$this->filterFlow($data))
+                return;
+
+            $modulus = date('s') % self::INTERVAL;
+
+            if ($modulus !== 0 || $this->collect) {
+                $this->flows[$data['device']]['bits'][] = $data['packet_size'] * $data['sampling_rate'];
+                if ($modulus !== 0)
+                    $this->collect = false;
+                return;
+            }
+
+            $flowps = count($this->flows[$data['device']]['bits']) / self::INTERVAL;
+            $bps = array_sum($this->flows[$data['device']]['bits']) / count($this->flows[$data['device']]['bits']);
+            $mbps = $bps / pow(1024, 2);
+
+            printf("%s: %0.2f Mb/s\t%s Flows/s\n", $data['device'], $mbps, $flowps);
+
+            $this->collect = true;
+
+            $this->flows[$data['device']]['bits'] = [];
+        }
+
+        private function filterFlow($data)
+        {
+            if (preg_match('/^(172\.16|10)\.*/', $data['src_ip']) ||
+                preg_match('/^(172\.16|10)\.*/', $data['dst_ip']))
+                return false;
+            return true;
+        }
+
+        private function filterDevice($data)
+        {
+            if (isset($this->options['device']) && $data['device'] != $this->options['device'])
+                return false;
+            return true;
+        }
+
+        private function filterInterface($data)
+        {
+            if (isset($this->options['interface']) && $data['interface'] != $this->options['interface'])
+                return false;
+            return true;
+        }
+
+        public function readInput()
+        {
+            printf("Collecting flows, polling interval %ss\n", self::INTERVAL);
+
+            if ($this->options['type'] == 'Cntr')
+                printf("%-12s %-15s %13s %27s\n",
+                    'IP', 'Interface', 'In', 'Out');
+
+            while ($csv = fgetcsv(STDIN)) {
+                $method = sprintf("process%s", ucfirst(strtolower($csv[0])));
+                array_shift($csv);
+
+                if ('process'.$this->options['type'] == $method)
+                    $this->$method($csv);
+            }
+        }
+
+
+    }
+
+    $sflow = new Sflow;
+    $sflow->readInput();
+
+    exit;
+
